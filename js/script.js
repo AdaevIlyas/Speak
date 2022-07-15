ef726f9626e564fb3a038525605762ebb2c07e09
AdaevIlyas <il_adaev@mail.ru>
2022-07-16 01:58:54 +0600

    add tabs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// active slider
 document.addEventListener('DOMContentLoaded', () => {
   new ItcSimpleSlider('#slider-one', {
     loop: true,
@@ -6,3 +7,46 @@ document.addEventListener('DOMContentLoaded', () => {
     swipe: true,
   });
 });
+
+// TABS
+
+document.addEventListener('DOMContentLoaded', function () {
+  // console.log(document.querySelectorAll('.how-we-work__step'))
+  document.querySelectorAll('.step').forEach(function (tabsBtn) {
+    // console.log(tabsBtn)
+    tabsBtn.addEventListener('click', function (event) {
+      const path = event.currentTarget.dataset.path
+
+      document.querySelectorAll('.tabs-info').forEach(function (tabContent) {
+        tabContent.classList.remove('tabs-info-active')
+      })
+      document.querySelector(`[data-target="${path}"]`).classList.add('tabs-info-active')
+    })
+  })
+  $(function () {
+    $('#step-one').click(function () {
+      $('#step-one').addClass('step-active');
+      $('#step-two').removeClass('step-active');
+      $('#step-three').removeClass('step-active');
+      $('#step-four').removeClass('step-active');
+    });
+    $('#step-two').click(function () {
+      $('#step-two').addClass('step-active');
+      $('#step-one').removeClass('step-active');
+      $('#step-three').removeClass('step-active');
+      $('#step-four').removeClass('step-active');
+    });
+    $('#step-three').click(function () {
+      $('#step-three').addClass('step-active');
+      $('#step-one').removeClass('step-active');
+      $('#step-two').removeClass('step-active');
+      $('#step-four').removeClass('step-active');
+    });
+    $('#step-four').click(function () {
+      $('#step-four').addClass('step-active');
+      $('#step-one').removeClass('step-active');
+      $('#step-two').removeClass('step-active');
+      $('#step-three').removeClass('step-active');
+    });
+  });
+})
